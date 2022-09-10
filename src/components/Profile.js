ff53f50f603114b51bfcea8eb7ed27575163c5ca
Megan McBride <meganambermcbride@gmail.com>
2022-09-09 23:29:01 -0400

    Add Redirect to profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,24 +1,29 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Route, Link, useRouteMatch } from "react-router-dom";
-import { selectCurrentUser, selectIsLoggedIn } from "../features/session/sessionSlice";
+import { Route, Link, useRouteMatch, Redirect } from "react-router-dom";
+import {
+  selectCurrentUser,
+  selectIsLoggedIn,
+} from "../features/session/sessionSlice";
 import EditProfileForm from "./EditProfileForm";
 
-export default function Profile () {
-  const currentUser = useSelector(selectCurrentUser)
+export default function Profile() {
+  const currentUser = useSelector(selectCurrentUser);
   const loggedIn = useSelector(selectIsLoggedIn);
 
- const {url, path} = useRouteMatch()
-  
-  // use loggedIn to return a Redirect
-  
+  const { url, path } = useRouteMatch();
+
+  if (!loggedIn) {
+    return <Redirect to="/sign-up" />;
+  }
+
   return (
     <main>
       <h1>{currentUser.username}</h1>
       <Link to={`${path}/edit`}>Edit</Link>
-      <Route path={`${url}/edit`} >
+      <Route path={`${url}/edit`}>
         <EditProfileForm />
       </Route>
     </main>
-  )
+  );
 }
